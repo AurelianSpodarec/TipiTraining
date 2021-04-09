145d2a1f9f405b5bd9e18bee1ee770fee9f8db25
aurelianspodarec <aurelianxspodarec@gmail.com>
2021-04-09 04:37:42 +0100

    Created block slim component with styles

--- themes/kayTheme/resources/views/blocks/block-slim-blocks.blade.php
+++ themes/kayTheme/resources/views/blocks/block-slim-blocks.blade.php
@@ -1,23 +1,23 @@
 <div class="flex flex-wrap lg:justify-between">
 
-    <div>   
-    <a class="flex align-center lg:justify-center">
-        <img class="w-8" src="https://storybrand.com/wp-content/uploads/2018/05/right-top-w.png" alt="">
-        <span class="font-bold uppercase text-white">Stream the Full 2-Day Workshop</span>
+    <div class="w-full lg:flex-1" style="background-color: #272B34;">   
+    <a class="flex items-center lg:justify-center py-7 px-12">
+        <img class="w-8 lg:w-12" style="max-height: 50px; max-width: 64px;" src="https://storybrand.com/wp-content/uploads/2018/05/right-top-w.png" alt="">
+        <span style="letter-spacing: 0.15em;" class="pl-5 font-bold uppercase text-white lg:text-lg">Stream the Full 2-Day Workshop</span>
     </a>
     </div>
 
-    <div>
-    <a class="flex align-center lg:justify-center">
-        <img class="w-8" src="https://storybrand.com/wp-content/uploads/2018/05/help-top-w.png" alt="">
-        <span class="font-bold uppercase text-white">Get Live Virtual Coaching</span>
+    <div class="w-full lg:flex-1" style="background-color: #22262F;">   
+    <a class="flex items-center lg:justify-center py-7 px-12">
+        <img class="w-8 lg:w-12" style="max-height: 50px; max-width: 64px;" src="https://storybrand.com/wp-content/uploads/2018/05/help-top-w.png" alt="">
+        <span style="letter-spacing: 0.15em;" class="pl-5 font-bold uppercase text-white lg:text-lg">Get Live Virtual Coaching</span>
     </a>
     </div>
 
-    <div>
-    <a class="flex align-center lg:justify-center">
-        <img class="w-8" src="https://storybrand.com/wp-content/uploads/2018/05/increase-top-w.png" alt="">
-        <span class="font-bold uppercase text-white">Know You’re Doing It Right</span>
+    <div class="w-full lg:flex-1" style="background-color: #171B23;">   
+    <a class="flex items-center lg:justify-center py-7 px-12">
+        <img class="w-8 lg:w-12" style="max-height: 50px; max-width: 64px;" src="https://storybrand.com/wp-content/uploads/2018/05/increase-top-w.png" alt="">
+        <span style="letter-spacing: 0.15em;" class="pl-5 font-bold uppercase text-white lg:text-lg">Know You’re Doing It Right</span>
     </a>
     </div>  
 
