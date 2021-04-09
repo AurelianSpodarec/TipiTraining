46e6e155dacf1fb81429e843214841608ae2e237
aurelianspodarec <aurelianxspodarec@gmail.com>
2021-04-09 06:21:57 +0100

    Updated block hero

--- themes/kayTheme/resources/views/blocks/block-hero.blade.php
+++ themes/kayTheme/resources/views/blocks/block-hero.blade.php
@@ -15,19 +15,19 @@ endif;
  
 ?>
 
-<div class="hero relative">
+<div class="hero relative py-32 bg-cover bg-no-repeat bg-center" style="background-image: url(https://storybrand.com/wp-content/uploads/2019/03/Watch-Video.jpg);">
 
     <video class="object-fit w-full h-full" data-id="2" muted="" autoplay="autoplay" loop="loop">
         <source src="https://player.vimeo.com/external/397303949.sd.mp4?s=24c722d4097e1a4fd7b830ca8992097c7f1d520f&amp;profile_id=165" type="video/mp4">
     </video>
 
-    <div class="absolute top-0 right-0 bottom-0 leftt-0">
-    <div class="flex h-full items-center justify-center flex-col text-center">
+    <div class="absolute top-0 right-0 bottom-0 left-0">
+    <div class="px-5 py-32 text-white lg:flex h-full lg:items-center justify-center flex-col lg:text-center">
         
-        <div><img alt="alt" src="https://storybrand.com/wp-content/uploads/2020/03/live-stream-icon-2.png" /></div>
-        <h1>{!! get_sub_field('title') !!}</h1>
-        <h2>{!! get_sub_field('subtitle') !!}</h2>
-        <div>{!! get_sub_field('subtitle_2') !!}</div>
+        <div><img class="mb-2" src="https://storybrand.com/wp-content/uploads/2020/03/live-stream-icon-2.png" alt="alt" /></div>
+        <h1 class="text-5xl font-bold mb-4">{!! get_sub_field('title') !!}</h1>
+        <h2 class="text-2xl mb-4">{!! get_sub_field('subtitle') !!}</h2>
+        <span class="text-3xl font-bold block mb-12">{!! get_sub_field('subtitle_2') !!}</span>
         <x-button variant="{{ $button_variant }}" kind="{{ $button_kind }}">{{ $button_text }}</x-button> 
 
 
