6add6e9fc7a817ce53725dfd1053919023253967
aurelianspodarec <aurelianxspodarec@gmail.com>
2021-04-15 18:57:03 +0100

    updates

--- themes/kayTheme/resources/views/blocks/block-faq-accordion.blade.php
+++ themes/kayTheme/resources/views/blocks/block-faq-accordion.blade.php
@@ -1,26 +1,26 @@
 <h2>FAQ</h2>
 
-
-<div class="accordion flex flex-col items-center justify-center">
+<div>
+<div class="accordion flex flex-col bg-white p-10 rounded-lg items-center justify-center">
    
-    <div class="w-full">
+    <div class="w-full mb-5">
         <input type="checkbox" name="panel" id="panel-1" class="hidden">
 
-        <label for="panel-1" class="relative block bg-black text-white p-4 shadow border-b border-grey">Will there be a replay available afterwards?</label>
-        <div class="accordion__content overflow-hidden bg-grey-lighter">
-          <p class="accordion__body p-4" id="panel1">Yes! All registrants will have access to a replay of the Livestream (not coaching breakout sessions) for two full weeks afterward. You will receive an email to the address you used during registration. That email will arrive within 48 hours of completion.</p>
+        <label for="panel-1" class="relative block bg-gray-200 text-black p-4 font-bold rounded-lg shadow border-b border-grey">Will there be a replay available afterwards?</label>
+        <div class="accordion__content overflow-hidden bg-grey-lighter border-r-2 rounded-b-lg border-b-2 border-l-2 border-gray">
+          <p class="accordion__body p-6" id="panel1">Yes! All registrants will have access to a replay of the Livestream (not coaching breakout sessions) for two full weeks afterward. You will receive an email to the address you used during registration. That email will arrive within 48 hours of completion.</p>
         </div>
     </div> 
 
-    <div class="w-full">
+    <div class="w-full mb-5">
         <input type="checkbox" name="panel" id="panel-2" class="hidden">
 
-        <label for="panel-2" class="relative block bg-black text-white p-4 shadow border-b border-grey">Panel 2</label>
-        <div class="accordion__content overflow-hidden bg-grey-lighter">
-          <h2 class="accordion__header pt-4 pl-4">Header</h2>
-          <p class="accordion__body p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur
+        <label for="panel-2" class="relative block bg-gray-200 text-black p-4 font-bold rounded-lg shadow border-b border-grey">Panel 2</label>
+        <div class="accordion__content overflow-hidden bg-grey-lighter border-r-2 rounded-b-lg border-b-2 border-l-2 border-gray">
+          <p class="accordion__body p-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto possimus at a cum saepe molestias modi illo facere ducimus voluptatibus praesentium deleniti fugiat ab error quia sit perspiciatis velit necessitatibus.Lorem ipsum dolor sit amet, consectetur
             adipisicing elit. Lorem ipsum dolor sit amet.</p>
         </div>
+    </div>
     </div>
     
 </div>
