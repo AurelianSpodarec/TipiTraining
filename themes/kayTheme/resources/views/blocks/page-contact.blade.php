057b9bd400d9559aff1b87c74740388863963aab
aurelianspodarec <aurelianxspodarec@gmail.com>
2021-05-02 18:03:02 +0100

    Set up initial contact form page layout

---
+++ themes/kayTheme/resources/views/blocks/page-contact.blade.php
@@ -0,0 +1,73 @@
+<div class="flex flex-wrap">
+
+
+    <div class="w-full lg:w-2/3 pr-8">
+        <h2>Send us a message! :)</h2>
+
+        <form class="p-10">
+        <div class="flex flex-wrap mb-6">
+
+            <div class="w-full md:w-1/2 pr-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                First Name
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane">
+                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+            </div>
+
+            <div class="w-full md:w-1/2 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                Last Name
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+                </div>
+            </div>
+
+            <div class="flex flex-wrap mb-6">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Email Address
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="********@*****.**">
+            </div>
+
+            <div class="flex flex-wrap mb-6">
+                <div class="w-full px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                Your Message
+                </label>
+                <textarea rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
+            </div>
+
+            <div class="flex justify-between w-full px-3">
+                <div class="md:flex md:items-center">
+                    <label class="block text-gray-500 font-bold">
+                    <input class="mr-2 leading-tight" type="checkbox">
+                        <span class="text-sm">
+                        Send me your newsletter!
+                        </span>
+                    </label>
+                </div>
+                <button class="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded" type="submit">
+                    Send Message
+                </button>
+            </div>
+
+
+        </div>
+        </form>
+    </div>
+
+
+    <div class="w-full lg:w-1/3">
+
+        <h3>Say Hello</h3>
+        <a href="mailto:email@gmail.com">email@gmail.com</a>
+
+        <h3>Call Kay</h3>
+        <a href="tel:0728272">0728472</a>
+        <p>alternitevaly schedule a call <a href="">here</a></p>
+
+    </div>
+
+
+</div>
